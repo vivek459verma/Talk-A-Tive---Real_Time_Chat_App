637fc379ea7f241f3ea4a7dbb1a29f44f26babf1
vivek verma <vivek459verma@gmail.com>
2024-03-31 14:35:32 +0530

    first

--- CHAT_APP_CLIENT/src/socket.js
+++ CHAT_APP_CLIENT/src/socket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client"; // Add this
 let socket;
 
 const connectSocket = (user_id) => {
-  socket = io("https://talk-a-tive-real-time-chat-app.onrender.com/", {
+  socket = io("https://talk-a-tive-real-time-chat-app.onrender.com", {
     query: `user_id=${user_id}`,
   });
 }; // Add this -- our server will run on port 4000, so we connect to it from here
